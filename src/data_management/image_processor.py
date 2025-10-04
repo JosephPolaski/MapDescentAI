ecba7a4197318608e54ab813e0718a7ebfda9b30
JosephPolaski <polaski.joseph@gmail.com>
2025-10-03 21:41:49 -0400

    Refactored image preprocessing to support pytorch tensors

--- src/data_management/image_processor.py
+++ src/data_management/image_processor.py
@@ -1,10 +1,12 @@
 import cv2
 import numpy as np
 import sys
+import torch
 
+from torch import Tensor
+from data_management.data_transfer_objects.feature_label_info import FeatureLabelInfo
 from data_management.data_manager import DataManager
 from data_management.enums.stored_data_type import StoredDataType
-from numpy.typing import NDArray
 from pathlib import Path
 from typing import Dict
 from utilities import constants
@@ -16,31 +18,48 @@ class ImageProcessor:
         self.data_manager = DataManager()           
         self.logger = MDLog()
         
-        self.features : NDArray = None
-        self.labels : None 
+        self.features_tensor : Tensor = None
+        self.labels_tensor : Tensor = None     
 
     def preprocess_image_data(self):
         try:
             self.logger.method_entry()
 
-            images_with_labels : Dict[str, Path] = self.data_manager.get_image_paths_with_labels()  
+            images_with_labels : Dict[str, Path] = self.data_manager.get_image_paths_with_labels()             
+           
+            data : FeatureLabelInfo = self.__get_feature_label_data(images_with_labels)      
 
-            number_of_classes = len(images_with_labels)
-            image_data = []
-            labels_as_integers = []
+            self.__set_tensor_data(data)      
 
-            for label, image_path_list in images_with_labels.items():               
-                for image_path in image_path_list: 
-                      
-                      normalized_image = cv2.imread(image_path).astype(np.float32) / 255.0
-                      image_data.append(normalized_image)
-                      labels_as_integers.append(constants.LABEL_TO_INDEX_MAP[label])
-
-            self.features = np.stack(image_data, axis=0)
-            self.labels =  np.eye(number_of_classes, dtype=np.uint8)[labels_as_integers]                   
             self.__split_and_store_data()
         except Exception as ex:
             self.logger.error(f"Failed to preprocess data: \n\n {ex} \n\n")    
+
+    def __get_feature_label_data(self, images_with_labels) -> FeatureLabelInfo:
+        try:
+            result = FeatureLabelInfo()
+
+            for label, image_path_list in images_with_labels.items():               
+                for image_path in image_path_list: 
+                        
+                    normalized_image = cv2.imread(image_path).astype(np.float32) / 255.0
+
+                    # Convert shape to match pytorch (Channels, Height, Width)
+                    normalized_image = np.transpose(normalized_image, (2,0,1))
+
+                    result.features_raw.append(normalized_image)
+                    result.labels.append(constants.LABEL_TO_INDEX_MAP[label])
+
+            return result
+        except Exception as ex:
+            self.logger.error(f"Failed getting feature and label data: \n\n {ex} \n\n") 
+    
+    def __set_tensor_data(self, data : FeatureLabelInfo) -> None:
+        features = np.stack(data.features_raw, axis=0)
+        self.features_tensor = torch.tensor(features)
+
+        labels = np.array(data.labels, dtype=np.int64)
+        self.labels_tensor = torch.tensor(labels, dtype=torch.long) 
 
     def __split_and_store_data(self):
         split_data = self.data_manager.split_dataset(self.labels, self.feature)
